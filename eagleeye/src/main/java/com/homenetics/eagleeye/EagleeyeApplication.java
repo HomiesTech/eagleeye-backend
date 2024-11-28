dc788a913b6b7ca3b8cbcf67f93c0c11f8f46426
Sanheen Sethi <sanheensethi37659@gmail.com>
2024-11-28 13:11:58 +0530

    basic Code

---
+++ eagleeye/src/main/java/com/homenetics/eagleeye/EagleeyeApplication.java
@@ -0,0 +1,13 @@
+package com.homenetics.eagleeye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleeyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleeyeApplication.class, args);
+	}
+
+}
---
+++ eagleeye/src/test/java/com/homenetics/eagleeye/EagleeyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homenetics.eagleeye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleeyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
